4c294bbf7e4607d7ddfc6dfd868e59b08db69596
shiyong.yin <1156546473@qq.com>
2018-07-26 07:32:29 +0000

    add hall.sh

---
+++ sh/hall.sh
@@ -0,0 +1,3 @@
+curl -O https://raw.githubusercontent.com/home-assistant/fabric-home-assistant/master/hass_rpi_installer.sh && sudo chown pi:pi hass_rpi_installer.sh && bash hass_rpi_installer.sh
+
+
