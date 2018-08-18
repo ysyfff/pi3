b23934a61c3030ee0c5233979cca0c433173c43b
ysyfff <ysyfff@qq.com>
2018-08-18 11:06:31 +0800

    Update init-pi.sh

--- sh/init-pi.sh
+++ sh/init-pi.sh
@@ -4,4 +4,8 @@ cat ~/.ssh/id_rsa.pub
 
 # 2. setup apt-get sourcelist
 
+# 3. install zsh
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+
+# 4. setup zsh
 
