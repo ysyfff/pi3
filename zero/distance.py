a07dc1d1cc42873e2f57046eba8ee90a329c3c1e
Shiyong Yin <braveyin168@outlook.com>
2018-08-24 21:30:45 +0800

    update distance

--- zero/distance.py
+++ zero/distance.py
@@ -1,15 +1,22 @@
-from gpiozero import DistanceSensor, LED
-from time import sleep
+#!/usr/bin/python
+# coding=utf-8
 
-sensor = DistanceSensor(21, 20)
+from gpiozero import DistanceSensor, LED
+from time import sleep, time
+
+sensor_outer = DistanceSensor(21, 20)
+sensor_inner = DistanceSensor(26, 19)
 led = LED(16)
 status = 'off'
 led.off()
 
 while True:
-    distance = sensor.distance * 100
-    print('Distance to nearest object is', distance, 'cm')
-    if distance < 45:
+    distance_inner = sensor_inner.distance * 100
+    distance_outer = sensor_outer.distance * 100
+    print('Distance outer', distance_outer, 'cm')
+    print('Distance inner', distance_inner, 'cm')
+    print('-------------------'*2)
+    if distance_outer < 45:
         if status == 'off':
             status = 'on'
             led.on()
